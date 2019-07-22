6e77747803b9acd0ab6a64c9a4b6e9e15f93f423
Thomas <1013454@student.sae.edu.au>
2019-07-22 14:58:46 +1000

    Made Scriptable objects for each of the creature cards

---
+++ Assets/Thomas/Card Designs/Scripts/Card.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Creature")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public string cardDescription;
+    public string cardType;
+
+    public int goldCost;
+    public int techCost;
+    public int attack;
+    public int health;
+
+
+}
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Spell")]
+public class SpellCard : ScriptableObject
+{
+    public string cardName;
+    public string cardDescription;
+    public string cardType;
+
+    public int goldCost;
+    public int techCost;
+    
+
+
+}
