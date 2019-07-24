7d50a1ef39c2c37ee673a0a957c871e66d0b9501
Tom Monaghan <105718@student.sae.edu.au>
2019-07-24 22:00:10 +1000

    Board updated to a 6x4, also added in a base

--- Assets/Monaghan/BoardManager.cs
+++ Assets/Monaghan/BoardManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+   
     private const float tileSize = 2.0f;
     private const float tileOffset = 1.0f;
     //To keep track of which tile you're selecting so you know which tile you're hovering over
@@ -12,29 +12,45 @@ public class BoardManager : MonoBehaviour
     private int selectionY = -1;
     private int tileRowNumber = 6;
     private int tileColumnNumber = 4;
-    
+    public float columnLineLength = 1.5f;
+    public float rowLineLength = 1.5f;
     
     private void Update()
     {
-        DrawChessboard();
+        UpdateSelection();
+        DrawGameboard();
     }
 
-    private void DrawChessboard()
+    private void DrawGameboard()
     {
-        Vector3 widthLine = Vector3.right * tileRowNumber;
-        Vector3 heightLine = Vector3.forward * tileColumnNumber;
+        Vector3 widthLine = (Vector3.right * tileRowNumber) * (rowLineLength / 3);
+        Vector3 heightLine = (Vector3.forward * tileColumnNumber) * (columnLineLength / 3) ;
 
         for (int i = 0; i <= tileRowNumber; i++)
         {
             Vector3 start = Vector3.forward * i;
-            Debug.DrawLine(start, start + widthLine,);
+            Debug.DrawLine(start, start + widthLine);
             for (int j = 0; j <= tileColumnNumber; j++)
             {
                 start = Vector3.right * j;
                 Debug.DrawLine(start, start +heightLine);
             }
         }
+
+       
     }
     
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+            return;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f,
+            LayerMask.GetMask("BoardPlane")))
+        {
+            Debug.Log(hit.point);
+        }
+    }
 
 }
