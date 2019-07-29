735859010553f15f550e41d6cd851af123463d91
Tom Monaghan <105718@student.sae.edu.au>
2019-07-29 10:15:45 +1000

    InheritanceDemo

---
+++ Assets/Monaghan/CreaturesCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreaturesCard : CardBase
+{
+    public int attack;
+    public int health;
+
+
+    public void PrintBase()
+    {
+        Debug.Log("Attack: " + attack);
+        Debug.Log("Health: " + health);
+        /*
+        Debug.Log("Card Name: " + cardName);
+        Debug.Log("Card Description: " + cardDescription);
+        Debug.Log("Card Type: " + cardType);
+        Debug.Log("Gold Cost: " + goldCost);
+        Debug.Log("Tech Cost: " + techCost);
+        */
+    }
+}
