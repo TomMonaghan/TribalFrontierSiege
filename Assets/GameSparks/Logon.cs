ad6a401bfc4afd619458fc6843ce5774038cbc3c
Tom Monaghan <105718@student.sae.edu.au>
2019-08-04 20:25:36 +1000

    GameSparks Initialize

---
+++ Assets/GameSparks/Logon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameSparks.Api.Requests;
+using GameSparks.Api.Responses;
+using GameSparks.Core;
+
+public class Logon : MonoBehaviour
+{
+    
+}
