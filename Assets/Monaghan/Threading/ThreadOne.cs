9eb06682df692310ead4bdf7ac581f472d30edfb
Tom Monaghan <105718@student.sae.edu.au>
2019-08-23 17:18:36 +1000

    Threading

---
+++ Assets/Monaghan/Threading/ThreadOne.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ThreadOne : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Finish first project");
+            DoSomeWork();
+            Debug.Log(("Finish Second Project"));
+        }
+    }
+
+    private void DoSomeWork()
+    {
+        Thread.Sleep(3000);
+        Debug.Log("Trimester over. Only finished one project");
+    }
+}
---
+++ Assets/Monaghan/Threading/ThreadTwo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ThreadTwo : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Finish first project");
+            Thread myThread = new Thread(new ThreadStart(DoSomeWork));
+            myThread.Start();
+            Debug.Log(("Finish second project"));
+
+        }
+    }
+
+    private void DoSomeWork()
+    {
+        Thread.Sleep(3000);
+        Debug.Log("Trimester over. Passed both projects");
+
+    }
+}
